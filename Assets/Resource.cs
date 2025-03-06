462741d888ae987a11eae6a35e08abc1e8a9f3fe
Erica <erica_wang@ucsb.edu>
2025-03-05 16:47:40 -0800

    adjust resource collection time

--- Assets/Resource.cs
+++ Assets/Resource.cs
@@ -4,7 +4,8 @@ public class Resource : MonoBehaviour
 {
     private SpriteRenderer sr;
 
-    private float time = 3.0f;
+    public float startTime = 1.5f;
+    private float time;
     private bool collecting;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,6 +13,7 @@ public class Resource : MonoBehaviour
     {
         collecting = false;
         sr = GetComponent<SpriteRenderer>();
+        time = startTime;
     }
 
     // Update is called once per frame
@@ -20,7 +22,7 @@ public class Resource : MonoBehaviour
         if(collecting)
             time -= Time.deltaTime;
             // taken from here: https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Color.Lerp.html
-            sr.color = Color.Lerp(Color.white, Color.red, Mathf.PingPong(3-time/3, 1));
+            sr.color = Color.Lerp(Color.white, Color.black, Mathf.PingPong((startTime-time)/startTime, 1));
         if(time <= 0) {
             GameObject.FindWithTag("ResourceSpawner").GetComponent<ResourceSpawner>().remove();
             Destroy(gameObject);
